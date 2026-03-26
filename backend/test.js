14afc32d28b83cfa7ad2f9273e4106114c2e6fa9
zerothree <issx404@proton.me>
2026-03-26 23:22:02 +0900

    cli init

--- backend/test.js
+++
@@ -1,9 +0,0 @@
-// test.js - файл с намеренными ошибками
-const unusedVariable = "test"; // Неиспользуемая переменная
-console.log(process.env.TEST); // process должен быть известен
-
-function test() {
-  let x = 1;
-  x = 2;
-  return x;
-}
